6515627223f4ebe776e224173020a3cf1b0c7203
Flores,Jhonathan <luis.jhonathan.flores@aptiv.com>
2024-11-20 11:30:45 -0600

    String validation function proposal

--- challenges/ValidSyntaxP/Submissions/Jhonathan_Flores_ValidParentheses.c
+++ challenges/ValidSyntaxP/Submissions/Jhonathan_Flores_ValidParentheses.c
@@ -2,20 +2,47 @@
 #include <string.h>
 
 bool isValid(char* s) {
-  unsigned int charCounter = 0;
-  
-  while('\0' != s[charCounter]){
-    if('(' != s[charCounter] && '{' != s[charCounter] && '[' != s[charCounter]){
-        return false;
+
+  char openSymbols[100];
+  char closeSymbols[100];
+
+  char auxDif = 0;
+  char auxCounter = 0;
+  char openCounter = 0;
+  char closeCounter = 0;
+
+  while('\0' != s[auxCounter] && '\r' != s[auxCounter] && '\n' != s[auxCounter]){
+    // Classify received string elements
+    if('(' == s[auxCounter] || '{' == s[auxCounter] || '[' == s[auxCounter]){
+        openSymbols[openCounter] = s[auxCounter];
+        openCounter++;
+    }
+    else if(')' == s[auxCounter] || ']' == s[auxCounter] || '}' == s[auxCounter]){
+        closeSymbols[closeCounter] = s[auxCounter];
+        closeCounter++;
     }
     else{
-      if(')' != s[charCounter+1] && ']' != s[charCounter+1] && '}' != s[charCounter+1]){
+        // Guard 1 to detect invalid element
         return false;
-      }
-      else{
-        charCounter += 2;
-      }
     }
+    auxCounter++;
   }
-  return true;
+
+  if(openCounter != closeCounter){
+    // Guard 2 to detect an unclosed symbol
+    return false;
+  }
+  else{
+    for(auxCounter=0;auxCounter<openCounter;auxCounter++){
+        auxDif = closeSymbols[auxCounter] - openSymbols[openCounter-auxCounter-1];
+        if(1 != auxDif && 2 != auxDif){
+            // Guard 3 to detect a mismatch in open/close symbols
+            return false;
+        }
+        else{
+        //   do nothing
+        }
+    }
+    return true;
+  }
 }
