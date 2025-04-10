ad999d012a1ca76598575d6badff5c5aa5f05c7e
René G <93566009+renegarciat@users.noreply.github.com>
2025-04-10 08:47:54 -0600

    Adding constant definitions

--- EPC_bsp_planA/ESP_BSP/motor_ctrl.cpp
+++ EPC_bsp_planA/ESP_BSP/motor_ctrl.cpp
@@ -1,5 +1,11 @@
 #include "motor_ctrl.hpp"
 
+/// @brief 
+/// @param motor 
+/// @param speed 
+/// @param direction 
+/// @return 
 bool motor_ctrl::set_motor_speed(motor_id_t motor, motor_speed_t speed, motor_direction_t direction) {
+  
   return false;
 }
--- EPC_bsp_planA/ESP_BSP/motor_ctrl.hpp
+++ EPC_bsp_planA/ESP_BSP/motor_ctrl.hpp
@@ -21,4 +21,13 @@ class motor_ctrl {
 public:
   bool set_motor_speed(motor_id_t motor, motor_speed_t speed, motor_direction_t direction);
 private:
+  const unsigned int hb1_pwm_ain1 = 34;
+  const unsigned int hb1_pwm_ain2 = 35;
+  const unsigned int hb1_pwm_bin1 = 32;
+  const unsigned int hb1_pwm_bin2 = 33;
+  const unsigned int hb2_pwm_ain1 = 25;
+  const unsigned int hb2_pwm_ain2 = 26;
+  const unsigned int hb2_pwm_bin1 = 27;
+  const unsigned int hb2_pwm_bin2 = 14;
+  
 };
