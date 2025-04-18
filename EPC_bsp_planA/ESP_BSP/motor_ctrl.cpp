9281c796d5db4c46275757c8cd8a9a33927b15cc
René G. <93566009+renegarciat@users.noreply.github.com>
2025-04-17 22:08:12 -0600

    Barely working!

--- EPC_bsp_planA/ESP_BSP/motor_ctrl.cpp
+++ EPC_bsp_planA/ESP_BSP/motor_ctrl.cpp
@@ -4,8 +4,9 @@ bool motor_ctrl::set_motor_speed(motor_id_t motor, motor_speed_t speed, motor_di
   /*NOTE: 
   * Be careful when calling this function multiple times in a short period of time, consider deadtime between calls to avoid shorting the motors.
   */
-  // Convert motor_speed_t to PWM value (0-255)
-  char pwm_value = _get_PWM_value(speed);
+  // Convert motor_speed_t to duty cycle value (0-255)
+  // uint8_t duty_cycle = map(speed, 0, 4, 0, 255); // Map speed to duty cycle (0-255)
+  uint32_t pwm_value = _get_PWM_value(speed);
   switch (motor) {
     case FRONT_LEFT:
       if (direction == FORWARD) { // Forward
@@ -14,7 +15,8 @@ bool motor_ctrl::set_motor_speed(motor_id_t motor, motor_speed_t speed, motor_di
       else { // Backward
         digitalWrite(DIRA,LOW);
       }
-      analogWrite(PWMA,pwm_value);
+      //analogWrite(PWMA,pwm_value);
+      ledcWrite(2, pwm_value); // Set PWM value for motor A
       break;
     case FRONT_RIGHT:
       if (direction) { // Forward
@@ -49,7 +51,7 @@ bool motor_ctrl::set_motor_speed(motor_id_t motor, motor_speed_t speed, motor_di
   }
 }
 
-char motor_ctrl::_get_PWM_value(motor_speed_t speed) {
+uint32_t motor_ctrl::_get_PWM_value(motor_speed_t speed) {
   switch (speed) {
     case OFF: return 0;
     case VERY_LOW_SPEED: return 50;
@@ -59,14 +61,25 @@ char motor_ctrl::_get_PWM_value(motor_speed_t speed) {
     default: return 0; // Invalid speed
   }
 }
+  // motor_ctrl::motor_ctrl(uint32_t frequency, uint8_t pwm_resolution) {
+  // this->frequency = frequency;
+  // this->pwm_resolution = pwm_resolution;
+  // // Configure the 4 PWM channels
+  // ledcSetup(pwm_pin0, frequency, pwm_resolution);
+  // ledcSetup(pwm_pin1, frequency, pwm_resolution);
+  // ledcSetup(pwm_pin2, frequency, pwm_resolution);
+  // ledcSetup(pwm_pin3, frequency, pwm_resolution);
+  // pinMode(DIRA, OUTPUT);
+  // }
 
   motor_ctrl::motor_ctrl() {
+  ledcAttach(2, frequency, pwm_resolution);
   pinMode(DIRA, OUTPUT);
-  pinMode(PWMA, OUTPUT);  
-  pinMode(DIRB, OUTPUT);
-  pinMode(PWMB, OUTPUT); 
-  pinMode(DIRC, OUTPUT);
-  pinMode(PWMC, OUTPUT);
-  pinMode(DIRD, OUTPUT);
-  pinMode(PWMD, OUTPUT);
+  // pinMode(PWMA, OUTPUT);  
+  // pinMode(DIRB, OUTPUT);
+  // pinMode(PWMB, OUTPUT); 
+  // pinMode(DIRC, OUTPUT);
+  // pinMode(PWMC, OUTPUT);
+  // pinMode(DIRD, OUTPUT);
+  // pinMode(PWMD, OUTPUT);
 }
--- EPC_bsp_planA/ESP_BSP/motor_ctrl.hpp
+++ EPC_bsp_planA/ESP_BSP/motor_ctrl.hpp
@@ -29,7 +29,7 @@ const motor_direction_t BACKWARD = false;
 
 // ESP32 GPIO pin definitions for motor control
 #define PWMA 15
-#define DIRA 32
+#define DIRA 0
 #define PWMD 3 
 #define DIRD 2 
 #define PWMC 5 
@@ -40,10 +40,17 @@ const motor_direction_t BACKWARD = false;
 class motor_ctrl {
 public:
   /**
-   * @brief Initializes the motor control pins.
-   * Sets the motor control pins to output mode and initializes them to low.
+   * @brief Default constructor for motor_ctrl class.
+   * Initializes the motor control with default frequency and PWM resolution.
    */
   motor_ctrl();
+
+  /**
+   * @brief Initializes the motor control with specified frequency and PWM resolution.
+   * @param frequency The frequency for PWM (default is 5 kHz).
+   * @param pwm_resolution The resolution for PWM (default is 8 bits).
+   */
+  motor_ctrl(uint32_t frequency, uint8_t pwm_resolution);
 
   /**
    * @brief Sets the speed and direction of a motor.
@@ -55,22 +62,28 @@ public:
   bool set_motor_speed(motor_id_t motor, motor_speed_t speed, motor_direction_t direction);
 
 private:
-  /*Note: GPIO should be between 1 and 16 */
-  const unsigned int hb1_pwm_ain1 = 15; //Corresponds to PWMA (pin 9 of shield) (pin D15 of ESP32) (GPIO15)
-  const unsigned int hb1_pwm_ain2 = 32; //Corresponds to DIRA (pin 8 of shield) (pin D2 of ESP32) (GPIO32)
-  const unsigned int hb1_pwm_bin1 = 32;
-  const unsigned int hb1_pwm_bin2 = 33;
-  const unsigned int hb2_pwm_ain1 = 25;
-  const unsigned int hb2_pwm_ain2 = 26;
-  const unsigned int hb2_pwm_bin1 = 27;
-  const unsigned int hb2_pwm_bin2 = 14;
+  /*Note: Channels run from 0 to 15 */
+  const unsigned int frequency = 5000; //5 KHz
+  const unsigned int hb1_pwm_ain1 = 34; //Corresponds to PWMA (pin 9 of shield) (pin D34 of ESP32) (GPIO?)
+  const unsigned int hb1_channel = 0 ;//Start from channel 0
+  const unsigned int pwm_resolution = 8; //[bits] Could be increased if needed.
+  uint8_t pwm_pin0 = 0; // PWM pin for motor 1 (left front)
+  // const unsigned int hb1_pwm_ain2 = 35;
+  // const unsigned int hb1_pwm_ain1 = 15; //Corresponds to PWMA (pin 9 of shield) (pin D34 of ESP32) (GPIO15)
+  // const unsigned int hb1_pwm_ain2 = 32; //Corresponds to DIRA (pin 8 of shield) (pin D2 of ESP32) (GPIO32)
+  // const unsigned int hb1_pwm_bin1 = 32;
+  // const unsigned int hb1_pwm_bin2 = 33;
+  // const unsigned int hb2_pwm_ain1 = 25;
+  // const unsigned int hb2_pwm_ain2 = 26;
+  // const unsigned int hb2_pwm_bin1 = 27;
+  // const unsigned int hb2_pwm_bin2 = 14;
 
   /**
    * @brief Converts the motor speed to a PWM value.
    * @param speed The desired motor speed (0-4). 
    * @return The corresponding PWM value (0-255).
    */
-  char _get_PWM_value(motor_speed_t speed);
+  uint32_t _get_PWM_value(motor_speed_t speed);
 };
 
 #endif // MOTOR_CTRL_HPP
